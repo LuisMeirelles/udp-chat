321870c0a15977b1318e08ee296c89ed0ffaab92
Luis Meirelles <luis.gustavo.souto.meirelles@gmail.com>
2025-12-31 19:30:42 -0300

    Initialize project structure with basic setup
    Includes:
    - Main entry points for `udp_server` and `udp_client`
    - CMake configuration for project build
    - Common header file
    - `.gitignore` for build and IDE files

---
+++ src/common.h
@@ -0,0 +1,8 @@
+//
+// Created by meirelles on 12/31/25.
+//
+
+#ifndef CHAT_COMMON_H
+#define CHAT_COMMON_H
+
+#endif //CHAT_COMMON_H
---
+++ src/server.c
@@ -0,0 +1,7 @@
+//
+// Created by meirelles on 12/31/25.
+//
+
+int main(void) {
+    return 0;
+}

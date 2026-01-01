97f806cd926b3839e0a3c4d1a60249d488279dfb
Luis Meirelles <luis.gustavo.souto.meirelles@gmail.com>
2026-01-01 04:36:19 -0300

    Refactor: define `BUFFER_SIZE` constant and improve message handling in UDP client and server

--- src/client.c
+++ src/client.c
@@ -9,6 +9,8 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+#define BUFFER_SIZE 1024
+
 int main(void) {
     const int fd = socket(AF_INET, SOCK_DGRAM, 0);
 
@@ -25,10 +27,10 @@ int main(void) {
     inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
 
     do {
-        char message[1024];
+        char message[BUFFER_SIZE] = {0};
 
-        fgets(message, sizeof(message), stdin);
+        const size_t read_bytes = read(STDIN_FILENO, message, BUFFER_SIZE);
 
-        sendto(fd, message, sizeof(message), 0, (const struct sockaddr *) &addr, sizeof(addr));
+        sendto(fd, &message, read_bytes, 0, (const struct sockaddr *) &addr, sizeof(addr));
     } while (1);
 }
--- src/server.c
+++ src/server.c
@@ -9,6 +9,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#define BUFFER_SIZE 1024
+
 int main(void) {
     const int fd = socket(AF_INET, SOCK_DGRAM, 0);
 
@@ -25,20 +27,28 @@ int main(void) {
 
     const int connection = bind(fd, (const struct sockaddr *) &addr, sizeof(addr));
 
-    const short error = errno;
+    const short bind_error = errno;
 
     if (connection == -1) {
         perror("An error occurred while trying to bind socket");
         close(fd);
 
-        return error;
+        return bind_error;
     }
 
-    char buf[1024];
-
     do {
-        memset(&buf, 0, 1024);
-        recv(fd, buf, 1024, 0);
-        printf("%s", buf);
+        char buf[BUFFER_SIZE] = {0};
+
+        const ssize_t n = recv(fd, buf, BUFFER_SIZE, 0);
+        const short recv_error = errno;
+
+        if (n == -1) {
+            perror("An error occurred while trying to receive a message from socket");
+            return recv_error;
+        }
+
+        /// Here, we separate the packets by `/n`, but in the future we will loop through the connections (perhaps using
+        /// `recv_from` and a connection pool?) so that we can detect `\0` at the end of the message.
+        write(STDOUT_FILENO, buf, n);
     } while (1);
 }

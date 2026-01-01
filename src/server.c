dfdb1815aae421097bb56c37e3a452b31ef9d6c8
Luis Meirelles <luis.gustavo.souto.meirelles@gmail.com>
2025-12-31 23:14:20 -0300

    Handle multiple UDP messages and add exit command logic

--- src/server.c
+++ src/server.c
@@ -4,6 +4,7 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -12,8 +13,7 @@ int main(void) {
     const int fd = socket(AF_INET, SOCK_DGRAM, 0);
 
     if (fd == -1) {
-        perror("An error occured when trying to start a socket");
-        close(fd);
+        perror("An error occurred when trying to start a socket");
         return errno;
     }
 
@@ -25,19 +25,20 @@ int main(void) {
 
     const int connection = bind(fd, (const struct sockaddr *) &addr, sizeof(addr));
 
+    const short error = errno;
+
     if (connection == -1) {
         perror("An error occurred while trying to bind socket");
         close(fd);
-        return errno;
+
+        return error;
     }
 
-    char buf[1024] = {0};
+    char buf[1024];
 
-    recvfrom(fd, buf, 1024, 0, (struct sockaddr *) &addr, (unsigned int *) sizeof(addr));
-
-    close(fd);
-
-    printf("%s", buf);
-
-    return 0;
+    do {
+        memset(&buf, 0, 1024);
+        recvfrom(fd, buf, 1024, 0, (struct sockaddr *) &addr, (unsigned int *) sizeof(addr));
+        printf("%s", buf);
+    } while (1);
 }

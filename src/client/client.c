dd18aebd76989bdbf27475926e6c5789053352cb
Luis Meirelles <luis.gustavo.souto.meirelles@gmail.com>
2026-01-01 09:32:57 -0300

    Refactor: replace input handling and socket initialization with `initialize_connection` in client logic to improve readability and consistency

--- src/client/client.c
+++ src/client/client.c
@@ -11,13 +11,9 @@
 #define BUFFER_SIZE 2
 
 int main(const int argc, const char **argv) {
-    const address input_address = handle_inputs(argc, argv);
+    const connection_config config = initialize_connection(argc, argv);
 
-    fprintf(stderr, "Running on %s:%d\n", input_address.ip, input_address.port);
+    fprintf(stderr, "Connecting to %s:%d\n", config.ip, config.port);
 
-    const int socket_file_descriptor = start_socket();
-
-    const struct sockaddr_in sock_addr = get_sock_addr(input_address);
-
-    run_client(socket_file_descriptor, sock_addr, BUFFER_SIZE);
+    run_client(config.fd, config.socket_address, BUFFER_SIZE);
 }
